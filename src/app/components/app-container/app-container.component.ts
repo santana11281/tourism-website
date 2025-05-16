81c1820d6381a78a3f56fef4a31574321bfbe566
Michael Santana <msantana@lacolonial.com.do>
2025-05-15 20:27:40 -0400

    added components

---
+++ src/app/components/app-container/app-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-container',
+  imports: [],
+  templateUrl: './app-container.component.html',
+  styleUrl: './app-container.component.css'
+})
+export class AppContainerComponent {
+
+}
---
+++ src/app/components/review-container/review-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-container',
+  imports: [],
+  templateUrl: './review-container.component.html',
+  styleUrl: './review-container.component.css'
+})
+export class ReviewContainerComponent {
+
+}
