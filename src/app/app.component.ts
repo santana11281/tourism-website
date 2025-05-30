71b62c70cef5c2d8fa3d110411f0c19587adcfcf
Michael Santana <msantana@lacolonial.com.do>
2025-05-30 09:45:31 -0400

    cambiar nombre de barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent,NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
